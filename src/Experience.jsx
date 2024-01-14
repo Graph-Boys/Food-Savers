6c0dfc9c357c19727a0edae059d77741a8dbeaec
Taylan Özdoğan <ozdogantaylan@gmail.com>
2024-01-14 21:41:56 +0300

    Level.js component.

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,5 +1,6 @@
 import { OrbitControls } from '@react-three/drei'
 import Lights from './Lights.jsx'
+import Level from "./Level.js";
 
 export default function Experience()
 {
@@ -8,21 +9,8 @@ export default function Experience()
         <OrbitControls makeDefault />
 
         <Lights />
+        <Level />
 
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" />
-        </mesh>
-
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
 
     </>
 }
