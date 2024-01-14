f3449b5e750d767b80e16bdeb4d4bababc310f0b
Taylan Özdoğan <ozdogantaylan@gmail.com>
2024-01-15 02:43:37 +0300

    Block spinner + rotation added.

--- src/Level.js
+++ src/Level.js
@@ -1,13 +1,44 @@
 import * as THREE from "three";
+import {RigidBody} from '@react-three/rapier'
+import {useRef} from "react";
+import {useFrame} from "@react-three/fiber";
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const floor1Material = new THREE.MeshStandardMaterial({color: "limegreen"});
+const floor2Material = new THREE.MeshStandardMaterial({color: "greenyellow"});
+const obstacleMaterial = new THREE.MeshStandardMaterial({color: "orangered"});
+const wallMaterial = new THREE.MeshStandardMaterial({color: "slategray"});
+
 
 function BlockStart({position = [0, 0, 0]}) {
     return <group position={position}>
         {/* Floor */}
-        <mesh geometry={boxGeometry} position={[0, -0.1, 0]} scale={[4, 0.2, 4]} receiveShadow>
-            <meshStandardMaterial color="limegreen"/>
-        </mesh>
+        <mesh geometry={boxGeometry} material={floor1Material} position={[0, -0.1, 0]} scale={[4, 0.2, 4]}
+              receiveShadow/>
+    </group>
+}
+
+function BlockSpinner({position = [0, 0, 0]}) {
+    const obstacle = useRef();
+
+    useFrame((state) => {
+        const time = state.clock.getElapsedTime();
+
+        const rotation = new THREE.Quaternion();
+        rotation.setFromEuler(new THREE.Euler(0, time, 0));
+        obstacle.current.setNextKinematicRotation(rotation);
+
+        console.log("time: " , time);
+    })
+
+    return <group position={position}>
+        {/* Floor */}
+        <mesh geometry={boxGeometry} material={floor2Material} position={[0, -0.1, 0]} scale={[4, 0.2, 4]}
+              receiveShadow/>
+        {/* Obstacle */}
+        <RigidBody ref={obstacle} type={"kinematicPosition"} position={[0, 0.3, 0]} restitution={0.2} friction={0}>
+            <mesh geometry={boxGeometry} material={obstacleMaterial} scale={[3.5, 0.3, 0.3]} castShadow receiveShadow/>
+        </RigidBody>
     </group>
 }
 
@@ -15,7 +46,8 @@ export default function Level() {
 
     return <>
 
-        <BlockStart position={[0, 0, 0]}/>
+        <BlockStart position={[0, 0, 4]}/>
+        <BlockSpinner position={[0, 0, 0]}/>
 
     </>
 }
